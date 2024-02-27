bc7e68f217fca2b3f4f9d3771c7b6ee9afc4b072
Ivanchik <Ivanchik@DESKTOP-8M3A9VQ>
2024-02-27 21:07:31 +0200

    Добавьте файлы проекта.

---
+++ Lab№1_2zavd_Latysh/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1_2zavd_Latysh
+{
+    class Program
+    {
+        public bool isAliquot(int x, int y)
+        {
+            return x % y == 0;
+        }
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            Console.Write("Введіть число, якому елементи масиву повинні бути кратні:");
+            int k = int.Parse(Console.ReadLine());
+            Console.WriteLine("Оберіть яким способом виконати задачу: \n 1. З Метод Where класу Enumerable\n 2. Власна реалізація,");
+            int x = int.Parse(Console.ReadLine());
+            Solution.MyDelegate myDelegate = null;
+            switch (x)
+            {
+                case 1: myDelegate = new Solution.MyDelegate(solution.Method1);
+                    break;
+                case 2: myDelegate = new Solution.MyDelegate(solution.Method2);
+                    break;
+            }
+            myDelegate(array, k);
+        }
+    }
+}
---
+++ Lab№1_2zavd_Latysh/Solution.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1_2zavd_Latysh
+{
+    class Solution
+    {
+        public delegate int[] MyDelegate(int[] array, int k);
+        public int[] Method1(int[] array, int k)
+        {
+            IEnumerable<int> query = array.Where(num => num % k == 0);
+            Console.WriteLine($"Масив з елементами, кратними {k}: [{string.Join(", ", query)}]");
+            return query.ToArray();
+        }
+        public int[] Method2(int[] array, int k)
+        {
+            string text = "fgdfghdfg";
+            int count = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % k == 0)
+                    count++;
+            }
+            int[] newArray = new int[count];
+            int index = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % k == 0)
+                {
+                    newArray[index] = array[i];
+                    index++;
+                }
+            }
+            Console.WriteLine($"Масив з елементами, кратними {k}: [{string.Join(", ", newArray)}]");
+            return newArray;
+        }
+    }
+}
